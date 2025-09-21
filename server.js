1e119e2f8d64474437b376e44be9dca171817e5c
TerickD <TerickMDupont@gmail.com>
2025-09-21 16:48:40 -0400

    cookbook lab complete

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js');
+const userController = require("./controllers/user.js")
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -39,18 +40,12 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.get('/vip-lounge', (req, res) => {
-//   if (req.session.user) {
-//     res.send(`Welcome to the party ${req.session.user.username}.`);
-//   } else {
-//     res.send('Sorry, no guests allowed.');
-//   }
-// });
 
 app.use(passUserToView);
 app.use('/auth', authController);
 app.use(isSignedIn);
 app.use("/users/:userId/foods", foodsController)
+app.use("/user", userController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
